1eb43c24b10516c16e75ca75dc6004508029ca41
Maite Kuhn <maikuhn@live.com>
2024-09-27 19:11:33 -0300

    Modificacion en tablas y readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # TPE_Web2
+Grupo 18 - Proyecto de control de compras
+
+Integrantes: Maite Kuhn - Josefina Socobehere
+
+Este proyecto consiste en realizar una base de datos que sirva para registrar las compras de mercaderia que se van a llevar a cabo para un local de indumentaria e identificar los proveedores de las mismas.
+
+Para que se cumpla la relacion de 1 a N, cada producto podra conseguirse solo en un proveedor, mientras que los proveedores podran tener mas de un producto. Las tablas estan relacionadas mediante la clave foranea id_proveedor_fk que se encuentra en la tabla productos y la clave primaria id_proveedor que se encuentra en la tabla proveedores.
+
+La tabla que se refiere a los productos ademas, llevara el control de cantidad, talle y valor de la prenda comprada, por unidad. La tabla del proveedor registra sus datos.
---
+++ tpe_web_2.sql
@@ -0,0 +1,93 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 14-09-2024 a las 01:03:32
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `tpe web 2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `id_producto` int(11) NOT NULL,
+  `Nombre_producto` varchar(50) NOT NULL,
+  `id_proveedor_fk` int(11) NOT NULL,
+  `categoría` varchar(100) NOT NULL
+  `cantidad` int(10) NOT NULL,
+  `talle` varchar(10) NOT NULL,
+  `valor` decimal(20,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedor`
+--
+
+CREATE TABLE `proveedor` (
+  `id_proveedor` int(11) NOT NULL,
+  `Nombre_proveedor` varchar(100) NOT NULL,
+  `medio_de_pago` varchar(100) NOT NULL,
+  `telefono` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`id_producto`),
+  ADD KEY `id_proveedor_fk` (`id_proveedor_fk`);
+
+--
+-- Indices de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  ADD PRIMARY KEY (`id_proveedor`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`id_proveedor_fk`) REFERENCES `proveedor` (`id_proveedor`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
